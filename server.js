9873241f2a0a68cb4cbb85c288408c59abf3cd36
ycleptkellan <yclepkellan@gmail.com>
2017-07-10 20:57:09 -0700

    style rules

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+/* eslint-disable no-console */
 
-const PORT = process.env.PORT || 8080;
+const http = require('http')
+const express = require('express')
+const compression = require('compression')
+const socket = require('socket.io')
 
-http.listen(3000);
+const app = express()
+const server = http.Server(app)
+const io = socket(server)
+
+server.use(compression())
+
+const PORT = process.env.PORT || 8080
+
+server.listen(PORT, () =>
+  console.log(`server listening at http://localhost:${PORT}`),
+)

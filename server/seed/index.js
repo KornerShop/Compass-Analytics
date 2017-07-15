22f8d345e73be3506cde47e1fbca7db030597ec1
ycleptkellan <yclepkellan@gmail.com>
2017-07-15 14:23:42 -0700

    formatting

--- server/seed/index.js
+++ server/seed/index.js
@@ -8,51 +8,60 @@ const generateLanguages = require('./languages');
 const generateOffices = require('./offices');
 const generateZips = require('./zips');
 
-const createDoc = (Model, doc) => {
-  return new Promise((resolve, reject) => {
-    new Model(doc).save((err, saved) => {
-      return err ? reject(err) : resolve(saved);
-    });
+const createDoc = (Model, doc) => new Promise((resolve, reject) => {
+    new Model(doc).save((err, saved) => err ? reject(err) : resolve(saved));
   });
-};
 
 const cleanDB = () => {
-  const promises = [Language, ZipCode, Office, Navigation]
-    .map(model => model.remove().exec());
-    console.log('...🌱...🌻...🌲');
-  return Promise.all(promises);
-}
-
+  const cleanPromises = [
+    Language,
+    ZipCode,
+    Office,
+    Navigation,
+  ].map(model => model.remove().exec());
+  console.log('...🌱...🌻...🌲');
+  return Promise.all(cleanPromises);
+};
 const createLanguages = data => {
-  console.log(`languages: ${JSON.stringify(generateZips(), null, 2)}`)
-  const promises = generateLanguages().map(lang => createDoc(Language, lang));
-  return Promise.all(promises)
-    .then(lang => Object.assign({languages: lang}, data || {}));
+  console.log(
+    `languages: ${JSON.stringify(generateZips(), null, 2)}`
+  );
+  const promises = generateLanguages().map(lang =>
+    createDoc(Language, lang)
+  );
+  return Promise.all(promises).then(lang =>
+    Object.assign({ languages: lang }, data || {})
+  );
 };
-
 const createNavs = data => {
-  console.log(`navs: ${JSON.stringify(generateNavs(), null, 2)}`)
-  const promises = generateNavs().map(nav => createDoc(Navigation, nav));
-  return Promise.all(promises)
-    .then(nav => Object.assign({navigation: nav}, data || {}));
+  console.log(`navs: ${JSON.stringify(generateNavs(), null, 2)}`);
+  const promises = generateNavs().map(nav =>
+    createDoc(Navigation, nav)
+  );
+  return Promise.all(promises).then(nav =>
+    Object.assign({ navigation: nav }, data || {})
+  );
 };
-
-const createOffices = data  => {
-  console.log(`offices: ${JSON.stringify(generateOffices(), null, 2)}`)
-  const promises = generateOffices().map(office => createDoc(Office, office));
-  return Promise.all(promises)
-    .then(office => Object.assign({officeChoosen: office}, data || {}));
-}
-
+const createOffices = data => {
+  console.log(
+    `offices: ${JSON.stringify(generateOffices(), null, 2)}`
+  );
+  const promises = generateOffices().map(office =>
+    createDoc(Office, office)
+  );
+  return Promise.all(promises).then(office =>
+    Object.assign({ officeChoosen: office }, data || {})
+  );
+};
 const createZipCode = data => {
   console.log(`zips: ${JSON.stringify(generateZips(), null, 2)}`);
   const promises = generateZips().map(zip => createDoc(ZipCode, zip));
-  return Promise.all(promises)
-    .then(zip => Object.assign({zipCode: zip}, data || {}));
-}
-
+  return Promise.all(promises).then(zip =>
+    Object.assign({ zipCode: zip }, data || {})
+  );
+};
 cleanDB()
   .then(createLanguages)
   .then(createNavs)
   .then(createOffices)
-  .then(createZipCode)
+  .then(createZipCode);

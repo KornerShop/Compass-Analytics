ae1cd04da902003e750c6525579b73a5d4a2e972
ycleptkellan <yclepkellan@gmail.com>
2017-07-21 17:55:37 -0700

    remove auth folder

--- server/auth/routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { verifyUser } = require('./auth');
-const { signIn } = require('./controller');
-
-router.post('/auth/login', verifyUser(), signIn);
-
-module.exports = router;

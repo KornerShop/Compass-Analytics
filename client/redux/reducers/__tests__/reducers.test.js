60491b92e9af4f24f69c634e8e500bab119d746c
Noel Parisi <ycleptnoel@gmail.com>
2017-07-29 21:41:38 -0700

    refactor reducer test code

--- client/redux/reducers/__tests__/reducers.test.js
+++ client/redux/reducers/__tests__/reducers.test.js
@@ -3,6 +3,7 @@ import initialState from "../../initialState";
 import * as actions from "../../actions/actions";
 
 let appState = initialState;
+let payload;
 
 describe("reducers", () => {
   test("@@INIT", () => {
@@ -10,206 +11,116 @@ describe("reducers", () => {
     expect(state).toEqual(appState);
   });
   test("TOGGLE_AUTHENTICATED", () => {
-    const state = rootReducer(appState, actions.toggleAuthenticated(true));
-    expect(state).toEqual({ ...appState, authenticated: true });
+    payload = true;
+    const state = rootReducer(appState, actions.toggleAuthenticated(payload));
+    expect(state).toEqual({ ...appState, authenticated: payload });
   });
   test("UPDATE_ERROR_MESSAGE", () => {
-    appState = { ...appState, authenticated: true };
-    const state = rootReducer(
-      appState,
-      actions.updateErrorMessage("No user with the given username")
-    );
+    appState = { ...appState, authenticated: payload };
+    payload = "No user with the given username";
+    const state = rootReducer(appState, actions.updateErrorMessage(payload));
     expect(state).toEqual({
       ...appState,
-      errorMessage: "No user with the given username"
+      errorMessage: payload
     });
   });
   test("TOGGLE_FETCHING", () => {
-    appState = { ...appState, errorMessage: "No user with the given username" };
-    const state = rootReducer(appState, actions.toggleFetching(true));
+    appState = { ...appState, errorMessage: payload };
+    payload = true;
+    const state = rootReducer(appState, actions.toggleFetching(payload));
     expect(state).toEqual({
       ...appState,
-      fetching: true
+      fetching: payload
     });
   });
   test("POPULATE_LANG_DATA", () => {
-    appState = { ...appState, fetching: true };
-    const state = rootReducer(
-      appState,
-      actions.populateLangData([
-        {
-          _id: "English",
-          value: 15
-        },
-        {
-          _id: "Spanish",
-          value: 27
-        }
-      ])
-    );
+    appState = { ...appState, fetching: payload };
+    payload = [
+      {
+        _id: "English",
+        value: 15
+      },
+      {
+        _id: "Spanish",
+        value: 27
+      }
+    ];
+    const state = rootReducer(appState, actions.populateLangData(payload));
     expect(state).toEqual({
       ...appState,
-      langData: [
-        {
-          _id: "English",
-          value: 15
-        },
-        {
-          _id: "Spanish",
-          value: 27
-        }
-      ]
+      langData: payload
     });
   });
   test("POPULATE_OFFICE_DATA", () => {
     appState = {
       ...appState,
-      langData: [
-        {
-          _id: "English",
-          value: 15
-        },
-        {
-          _id: "Spanish",
-          value: 27
-        }
-      ]
+      langData: payload
     };
-    const state = rootReducer(
-      appState,
-      actions.populateOfficeData([
-        {
-          _id: {
-            office: "WIC",
-            date: "7/28/2017"
-          },
-          count: 1
+    payload = [
+      {
+        _id: {
+          office: "WIC",
+          date: "7/28/2017"
         },
-        {
-          _id: {
-            office: "SNAP",
-            date: "7/29/2017"
-          },
-          count: 3
-        }
-      ])
-    );
+        count: 1
+      },
+      {
+        _id: {
+          office: "SNAP",
+          date: "7/29/2017"
+        },
+        count: 3
+      }
+    ];
+    const state = rootReducer(appState, actions.populateOfficeData(payload));
     expect(state).toEqual({
       ...appState,
-      officeData: [
-        {
-          _id: {
-            office: "WIC",
-            date: "7/28/2017"
-          },
-          count: 1
-        },
-        {
-          _id: {
-            office: "SNAP",
-            date: "7/29/2017"
-          },
-          count: 3
-        }
-      ]
+      officeData: payload
     });
   });
   test("POPULATE_NAV_DATA", () => {
     appState = {
       ...appState,
-      officeData: [
-        {
-          _id: {
-            office: "WIC",
-            date: "7/28/2017"
-          },
-          count: 1
-        },
-        {
-          _id: {
-            office: "SNAP",
-            date: "7/29/2017"
-          },
-          count: 3
-        }
-      ]
+      officeData: payload
     };
-    const state = rootReducer(
-      appState,
-      actions.populateNavData([
-        {
-          _id: {
-            office: "WIC",
-            date: "7/28/2017"
-          },
-          count: 1
+    payload = [
+      {
+        _id: {
+          office: "WIC",
+          date: "7/28/2017"
         },
-        {
-          _id: {
-            office: "SNAP",
-            date: "7/29/2017"
-          },
-          count: 3
-        }
-      ])
-    );
+        count: 1
+      },
+      {
+        _id: {
+          office: "SNAP",
+          date: "7/29/2017"
+        },
+        count: 3
+      }
+    ];
+
+    const state = rootReducer(appState, actions.populateNavData(payload));
     expect(state).toEqual({
       ...appState,
-      navData: [
-        {
-          _id: {
-            office: "WIC",
-            date: "7/28/2017"
-          },
-          count: 1
-        },
-        {
-          _id: {
-            office: "SNAP",
-            date: "7/29/2017"
-          },
-          count: 3
-        }
-      ]
+      navData: payload
     });
   });
   test("POPULATE_ZIP_DATA", () => {
     appState = {
       ...appState,
-      navData: [
-        {
-          _id: {
-            office: "WIC",
-            date: "7/28/2017"
-          },
-          count: 1
-        },
-        {
-          _id: {
-            office: "SNAP",
-            date: "7/29/2017"
-          },
-          count: 3
-        }
-      ]
+      navData: payload
     };
-    const state = rootReducer(
-      appState,
-      actions.populateZipData([
-        {
-          _id: 95404,
-          count: 2
-        }
-      ])
-    );
+    payload = [
+      {
+        _id: 95404,
+        count: 2
+      }
+    ];
+    const state = rootReducer(appState, actions.populateZipData(payload));
     expect(state).toEqual({
       ...appState,
-      zipData: [
-        {
-          _id: 95404,
-          count: 2
-        }
-      ]
+      zipData: payload
     });
   });
 });
